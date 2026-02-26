37a243ffb0340ed16ee78b9df8ae411833d5498d
s-uhyunkim <suhyun.kim@protonmail.com>
2026-02-26 13:02:06 -0500

    Further specified `SignaturePad` model

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
     TODO: Replace
 """
 
-from fastapi import FastAPI, Request, status
+from typing import List, Dict, Any
+
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class SignaturePad(BaseModel):
-    array: object
+    array: List[Dict[str, Any]] = Field(default_factory=list)
+    # "= []" or "= Field(default=[])" work, but `default_factory` is recommended for explicit, instance-unique defaults
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -28,8 +31,7 @@ async def read_root(request: Request):
 async def get_data(signature_pad: SignaturePad):
     global SIGNATURE_PAD
     SIGNATURE_PAD = signature_pad
-    print(SIGNATURE_PAD)
-    return RedirectResponse(url="/output", status_code=303)
+    return RedirectResponse(url="/output", status_code=302)
 
 @app.get("/output")
 async def read_output(request: Request):
--- static/app.js
+++ static/app.js
@@ -3,17 +3,16 @@ const signaturePad = new SignaturePad(canvas);
 const connectStrokeButton = document.getElementById("connect-strokes");
 
 connectStrokeButton.addEventListener("click", async () => {
-    const temp = signaturePad.toData();
+    console.log(signaturePad.toData());
+
     const response = await fetch("/signature-pad-data", {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({array: temp}) // object?
+        body: JSON.stringify({array: signaturePad.toData()})
     });
 
-    if (!response.ok) {
+    if (!response.ok)
         console.error("Failed to send signaturePad data:", response.status);
-        return;
-    }
 
     window.location.href = "/output";
 });
