3d656c530d6a70340d052466773becd0f0d7163b
s-uhyunkim <suhyun.kim@protonmail.com>
2026-02-26 11:48:58 -0500

    Add `POST` endpoint for `signaturePad` data and `/output` page
    - Add `output.html`
    - Update `requirements.txt`

--- main.py
+++ main.py
@@ -2,17 +2,36 @@
     TODO: Replace
 """
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+
+class SignaturePad(BaseModel):
+    array: object
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+SIGNATURE_PAD = None
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
-def main(request: Request):
+async def read_root(request: Request):
     """Return a ``Coroutine`` of the ``index.html`` template and the ``request`` value."""
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/signature-pad-data")
+async def get_data(signature_pad: SignaturePad):
+    global SIGNATURE_PAD
+    SIGNATURE_PAD = signature_pad
+    print(SIGNATURE_PAD)
+    return RedirectResponse(url="/output", status_code=303)
+
+@app.get("/output")
+async def read_output(request: Request):
+    """Return a ``Coroutine`` of the ``output.html`` template and the ``request`` and ``signature_pad`` values."""
+    return templates.TemplateResponse("output.html", {"request": request, "signature_pad": SIGNATURE_PAD})
--- static/app.js
+++ static/app.js
@@ -1,3 +1,19 @@
-const canvas = document.querySelector("canvas");
-
+const canvas = document.getElementById("input");
 const signaturePad = new SignaturePad(canvas);
+const connectStrokeButton = document.getElementById("connect-strokes");
+
+connectStrokeButton.addEventListener("click", async () => {
+    const temp = signaturePad.toData();
+    const response = await fetch("/signature-pad-data", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({array: temp}) // object?
+    });
+
+    if (!response.ok) {
+        console.error("Failed to send signaturePad data:", response.status);
+        return;
+    }
+
+    window.location.href = "/output";
+});
